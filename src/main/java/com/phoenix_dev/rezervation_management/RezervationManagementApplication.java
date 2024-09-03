dde7ca442d97800825eb69843d0589c193871938
CankatCS <68610552+CankatCS@users.noreply.github.com>
2024-09-03 21:29:56 +0300

    Initial commit

---
+++ src/main/java/com/phoenix_dev/rezervation_management/RezervationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.phoenix_dev.rezervation_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezervationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezervationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phoenix_dev/rezervation_management/RezervationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix_dev.rezervation_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezervationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
